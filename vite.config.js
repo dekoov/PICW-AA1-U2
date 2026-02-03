2bfc58b878be496b3811f00c907edd99438be669
dekoov <dcobel@hotmail.com>
2026-02-02 20:07:50 -0500

    weather api

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        proxy: {
+            // Esto simula el comportamiento de Vercel en local
+            '/api/weather': {
+                target: 'https://api.openweathermap.org/data/2.5/weather',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api\/weather/, ''),
+            },
+        },
+    },
 })
